8b7c010f0607803e53c0271011570f06439a9f07
Mateusz Paprocki <mattpap@gmail.com>
2011-07-10 06:21:04 +0200

    Update

--- html/tutorial-1.py
+++ html/tutorial-1.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-from sympy.mpmath import cplot
+from sympy.mpmath import plot, log
 
 fig = plt.figure()
 axes = fig.add_subplot(111)
-axes.set_title("Density plot of $x^3 - 1$ in the complex plane.")
+axes.set_title(r"Plot of $f(x)$ in $[0, 0.01]$.")
 
-cplot(lambda x: x**3 - 1, re=[-2, 2], im=[-2, 2], axes=axes)
+f = lambda x: x**(1 - log(log(log(log(1/x)))))
+plot(f, xlim=[0, 0.01], axes=axes)
