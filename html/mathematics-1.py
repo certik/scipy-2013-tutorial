eabf34d6b455c668381ae38bdf7a27ec15d10115
Aaron Meurer <asmeurer@gmail.com>
2013-06-22 15:39:02 -0500

    Update

--- html/mathematics-1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from sympy.mpmath import plot, log
-
-fig = plt.figure()
-axes = fig.add_subplot(111)
-axes.set_title(r"Plot of $f(x)$ in $[0, 0.01]$.")
-
-f = lambda x: x**(1 - log(log(log(log(1/x)))))
-plot(f, xlim=[0, 0.01], axes=axes)
